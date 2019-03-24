cea346debf6a74df90a8b8d4cdbebf1f40c1edbb
M.V.S. SRIHARSHA <sriharshamvs@gmail.com>
2019-03-24 12:18:30 +0530

    Updated Hardware files

--- Hardware/MasterDevice/MasterDevice.ino
+++ Hardware/MasterDevice/MasterDevice.ino
@@ -1,20 +1,29 @@
 #include <Wire.h>
 
+#define I2C_Request_interval  3000
+
+unsigned long I2C_Request_previousMillis = 0;       
+
 void setup() {
   Serial.begin(115200); /* begin serial for debug */
   Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
 }
 
 void loop() {
-//  Wire.beginTransmission(8); /* begin with device address 8 */
-//  Wire.write("Hello Arduino");  /* sends hello string */
-//  Wire.endTransmission();    /* stop transmitting */
+  unsigned long I2C_Request_currentMillis = millis();
+  if (I2C_Request_currentMillis - I2C_Request_previousMillis >= I2C_Request_interval) {
+    I2C_Request_previousMillis = I2C_Request_currentMillis;
+    I2C_Request_Data();
+    Serial.println();
+    //delay(3000);
+  }
+  //Serial.println();
+}
 
-  Wire.requestFrom(8, 32); /* request & read data of size 13 from slave */
+void I2C_Request_Data(void){
+  Wire.requestFrom(8, 32); /* request & read data of size 32 from slave */
   while(Wire.available()){
     char c = Wire.read();
     Serial.print(c);
   }
-  Serial.println();
-  delay(1000);
 }
--- Hardware/SlaveDevice/SlaveDevice.ino
+++ Hardware/SlaveDevice/SlaveDevice.ino
@@ -5,13 +5,16 @@
 #define DHTPIN  5
 #define RTC_SDA A1
 #define RTC_SCL A0
+#define ReadSensors_interval  3000
 
-dht DHT;
-DS3231  rtc(RTC_SDA, RTC_SCL);
+unsigned long ReadSensors_previousMillis = 0;       
 
 unsigned char temp=0, humid=0;
 String comString="";
 char comBuffer[20];
+
+dht DHT;
+DS3231  rtc(RTC_SDA, RTC_SCL);
 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
@@ -19,9 +22,17 @@ void setup() {
   Wire.onRequest(requestEvent); // register request event
   rtc.begin();
   Serial.begin(115200);           // start serial for debug
-}
+} // void setup ends here
 
 void loop() {
+  unsigned long ReadSensors_currentMillis = millis();
+  if (ReadSensors_currentMillis - ReadSensors_previousMillis >= ReadSensors_interval) {
+    ReadSensors_previousMillis = ReadSensors_currentMillis;
+    ReadSensors_Data();
+  }
+} // void loop ends here
+
+void ReadSensors_Data(void){
   DHT.read11(DHTPIN);
   temp = DHT.temperature;
   humid = DHT.humidity;
@@ -29,22 +40,8 @@ void loop() {
   comString = comString + "," + rtc.getDOWStr() + "," + rtc.getDateStr() + "," + rtc.getTimeStr();
   comString = comString +'\0';
   Serial.println(comString);
-  //DisplayTime();
   comString.toCharArray(comBuffer,comString.length());
-    
-  delay(3000);
 }
-
-/*
-//function that executes whenever data is received from master
-void receiveEvent(int howMany) {
-  while (0 < Wire.available()) {
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
-  }
-  Serial.println();         // to newline
-}
-*/
 
 // function that executes whenever data is requested from master
 void requestEvent() {
@@ -58,4 +55,13 @@ void requestEvent() {
   Serial.print(" -- ");
   Serial.println(rtc.getTimeStr()); // Send time
   delay (1000); // Wait one second before repeating :)
+}
+
+//function that executes whenever data is received from master
+void receiveEvent(int howMany) {
+  while (0 < Wire.available()) {
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  Serial.println();         // to newline
 }*/
